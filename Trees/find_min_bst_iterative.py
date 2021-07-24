a3d34680f340c45d6b21dee555b18bd1cb3fccad
Chirag Trivedi <chirag.trivedi@gmail.com>
2021-07-24 18:43:44 -0400

    find min in bst

---
+++ Trees/find_min_bst_iterative.py
@@ -0,0 +1,52 @@
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def treeInput():
+    rootData = int(input())
+
+    if rootData == -1:
+        return None
+
+    root = BinaryTreeNode(rootData)
+    leftTree = treeInput()
+    rightTree = treeInput()
+    root.left = leftTree
+    root.right = rightTree
+    return root
+
+def printTree(root):
+    if root is None:
+        return 
+
+    print(root.data,end=":")
+    if root.left:
+        print("Left=",root.left.data,end=",")
+    else:
+        print("Left=",None,end=",")
+    if root.right:
+        print("Right=",root.right.data)
+    else:
+        print("Right=",None)
+
+    printTree(root.left)
+    printTree(root.right)
+
+def find_min(root):
+
+    if root is None:
+        return float('inf')
+
+    curr = root
+
+    while curr.left is not None:
+        curr = curr.left
+
+    return curr.data
+
+
+root = treeInput()
+printTree(root)
+print(find_min(root))
